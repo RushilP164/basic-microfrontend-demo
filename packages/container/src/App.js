edb4bf49caa0b5b8a60cd5388fb19671f56b289c
Rushil Patel <rushil.patel.se@gmail.com>
2024-07-26 22:22:18 +0530

    container index

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,5 +2,11 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-	return <MarketingApp />;
+	return (
+		<>
+			<h2>Hello there!</h2>
+			<hr />
+			<MarketingApp />
+		</>
+	);
 };
